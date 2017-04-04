aef77beced45d86967bde0df62c814269f78b471
Kevin Tyrrell <kev070892@gmail.com>
2017-04-04 14:02:03 -0700

    Initial commit.

---
+++ Random.c
@@ -0,0 +1,64 @@
+﻿
+#include "Random.h"
+
+/* Local functions. */
+static unsigned int rand_KISS();
+
+/* Seed variables for this generator. */
+static unsigned int x = 123456789,
+y = 362436000,
+z = 521288629,
+c = 7654321;
+
+/*
+* Seeds this pseudo-random number generator with a provided value.
+* If a seed is not provided, random values will still be safe to use.
+* All values of [0, UINT_MAX] are safe to use.
+* Ω(1), Θ(1), O(1)
+*/
+void rand_seed(const unsigned int seed)
+{
+	x += seed;
+}
+
+/*
+* Generates a pseudo-random unsigned integer value from the current sequence.
+* See: `KISS` documentation.
+* Ω(1), Θ(1), O(1)
+*/
+unsigned int rand_next()
+{
+	return rand_KISS();
+}
+
+/*
+* Keep It Simple Stupid (KISS)
+* Source: https://en.wikipedia.org/wiki/KISS_(algorithm)
+* y, z, and c cannot be set to zero.
+* Ω(1), Θ(1), O(1)
+*/
+static unsigned int rand_KISS()
+{
+	const unsigned long long a = 698769069ULL;
+	unsigned long long t;
+
+	x = 69069 * x + 12345;
+
+	y ^= (y << 13);
+	y ^= (y >> 17);
+	y ^= (y << 5);
+
+	t = a*z + c;
+	c = (t >> 32);
+
+	return x + y + (z = t);
+}
+
+/*
+* Returns a pseudo-random unsigned integer in the domain of [0,limit).
+* Ω(1), Θ(1), O(1)
+*/
+unsigned int rand_limit(const unsigned int limit)
+{
+	return rand_KISS() % limit;
+}
---
+++ Random.h
@@ -0,0 +1,15 @@
+
+/*
+Author: Kevin Tyrrell
+Date: 4/04/2017
+Version: 1.0
+*/
+
+#pragma once
+
+/* Seeds this pseudo-random number generator with a provided value. */
+void rand_seed(const unsigned int seed);
+/* Generates a pseudo-random unsigned integer value from the current sequence. */
+unsigned int rand_next();
+/* Returns a pseudo-random unsigned integer in the domain of [0,limit). */
+unsigned int rand_limit(const unsigned int limit);
